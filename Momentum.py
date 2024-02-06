39225867eece7d9a5542048a6f2d3649d0107b14
XZaviorG <156961296+XZaviorG@users.noreply.github.com>
2024-02-06 14:04:34 -0800

    GP_01_PE_2-5.py

--- Momentum.py
+++ Momentum.py
@@ -1,7 +1,3 @@
-GP_01_PE_2-5 (Chapter 2: Programming Exercise 2-5)
-
-Task 1: Write a program in the file momentum.py that accepts an object’s mass (in kilograms) and velocity (in meters per second) as inputs and then outputs its momentum. (LO: 2.3, 2.4)
-
 print("This program calculates an object's momentum.")
 mass = float(input("Enter mass in Kg:"))
 velocity = int(input("Enter velocity in m/s:"))
